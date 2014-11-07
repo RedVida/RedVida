c631c8ebb4d5d8c5a5b1a554077c186b876bafff
Francisco Barra Sepúlveda <franciscobarra.sep@gmail.com>
2014-11-07 03:27:00 -0300

    b
    b

--- themes/semantic/views/donantes/_form.php
+++ themes/semantic/views/donantes/_form.php
@@ -17,49 +17,75 @@
 
 	'enableAjaxValidation'=>false,
 )); ?>
+
+<?php Yii::app()->clientScript->registerScript(
+    'myHideEffect',
+    '$(".errors").animate({opacity: 1.0}, 5000).fadeOut("slow");',
+    CClientScript::POS_READY
+); ?>
         
 	<?php echo $form->errorSummary($model); ?>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'nombres'); ?>
-		<?php echo $form->textField($model,'nombres',array('size'=>30,'maxlength'=>128)); ?>
-		<?php echo $form->error($model,'nombres'); ?>
+<div class="ui form">
+   <div class="fields">
+	 	<div class="fields four wide field">
+			<?php echo $form->labelEx($model,'nombres'); ?>
+			<?php echo $form->textField($model,'nombres',array('size'=>30,'maxlength'=>128)); ?>
+			<div class="errors">
+			<?php echo $form->error($model,'nombres',array('class' => 'ui small red pointing above ui label')); ?>
+			</div>
+		</div>
 	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'apellidos'); ?>
-		<?php echo $form->textField($model,'apellidos',array('size'=>30,'maxlength'=>128)); ?>
-		<?php echo $form->error($model,'apellidos'); ?>
+	<div class="fields">
+		<div class="four wide field">
+			<?php echo $form->labelEx($model,'apellidos'); ?>
+			<?php echo $form->textField($model,'apellidos',array('size'=>30,'maxlength'=>128)); ?>
+			<div class="errors">
+			<?php echo $form->error($model,'apellidos',array('class' => 'ui small red pointing above ui label')); ?>
+		    </div>
+		</div>
 	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'rut'); ?>
-		<?php echo $form->textField($model,'rut',array('size'=>30,'maxlength'=>12,'id'=>'rut')); ?>
-		<?php echo $form->error($model,'rut'); ?>
+	<div class="fields">
+		<div class="four wide field">
+			<?php echo $form->labelEx($model,'rut'); ?>
+			<?php echo $form->textField($model,'rut',array('size'=>30,'maxlength'=>12,'id'=>'rut')); ?>
+			<div class="errors">
+			<?php echo $form->error($model,'rut',array('class' => 'ui small red pointing above ui label')); ?>
+			</div>
+		</div>
+    </div>
+    <div class="fields">    
+        <div class="four wide field">
+			<?php echo $form->labelEx($model,'email'); ?>
+			<?php echo $form->textField($model,'email',array('size'=>30,'maxlength'=>128)); ?>
+			<div class="errors">
+			<?php echo $form->error($model,'email',array('class' => 'ui small red pointing above ui label')); ?>
+			</div>
+		</div>
+    </div>
+    <div class="fields">     
+        <div class="four wide field">
+			<?php echo $form->labelEx($model,'num_contacto'); ?>
+			<?php echo $form->textField($model,'num_contacto',array('size'=>30,'maxlength'=>128)); ?>
+			<div class="errors">
+			<?php echo $form->error($model,'num_contacto',array('class' => 'ui small red pointing above ui label')); ?>
+			</div>
+		</div>
+    </div> 
+    <div class="fields">    
+        <div class="four wide field">
+			<?php echo $form->labelEx($model,'direccion'); ?>
+			<?php echo $form->textField($model,'direccion',array('size'=>30,'maxlength'=>128)); ?>
+			<div class="errors">
+			<?php echo $form->error($model,'direccion',array('class' => 'ui small red pointing above ui label')); ?>
+			</div>
+		</div>
 	</div>
+</div>   
+        <div class="four wide field">
         
-        <div class="row">
-		<?php echo $form->labelEx($model,'email'); ?>
-		<?php echo $form->textField($model,'email',array('size'=>30,'maxlength'=>128)); ?>
-		<?php echo $form->error($model,'email'); ?>
-	</div>
-        
-        <div class="row">
-		<?php echo $form->labelEx($model,'num_contacto'); ?>
-		<?php echo $form->textField($model,'num_contacto',array('size'=>30,'maxlength'=>128)); ?>
-		<?php echo $form->error($model,'num_contacto'); ?>
-	</div>
-        
-        <div class="row">
-		<?php echo $form->labelEx($model,'direccion'); ?>
-		<?php echo $form->textField($model,'direccion',array('size'=>30,'maxlength'=>128)); ?>
-		<?php echo $form->error($model,'direccion'); ?>
-	</div>
-        
-        <div class="row">
-        <?php
-        echo $form->labelEx($model,'Tipo De Sangre');
-        echo $form->radioButtonList($model,'tipo_sangre',
+       <?php echo $form->labelEx($model,'Tipo De Sangre'); ?>
+       <br>
+       <?php echo $form->radioButtonList($model,'tipo_sangre', 
                 array('A' => 'A',
                       'B' => 'B',
                       'AB' => 'AB',
@@ -68,10 +94,10 @@
                     'labelOptions'=> array('style' => 'display:inline'),
                       'separator' => '',
                       'template' => ' {label}:  {input} ',
+                      'class' => 'ui radio checkbox'
                     )
          );
         ?>
-<<<<<<< HEAD
 </div>
 </div>
        </div> 
@@ -82,21 +108,8 @@
 	<br><br>
 	<div class="row buttons">
 	    <?php echo CHtml::submitButton(CrugeTranslator::t('login', "Login"),array("class"=>"ui blue submit button")); ?>
-=======
-    </div>
-        
-    <div class="row">
-		<?php 
-        echo $form->labelEx($model,'Centro Medico');
-		echo $form->dropDownList($model,'id_centro_medico', 
-		CHtml::listData(CentroMedico::model()->findAll(), 'id', 'nombre')); ?>
 	</div>
 
-	<div class="row buttons">
-		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
->>>>>>> origin/master
-	</div>
-     <br>
 
 
 <?php $this->endWidget(); ?>
