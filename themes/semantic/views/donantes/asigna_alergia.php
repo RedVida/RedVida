0769fea27934786a051083f2bfdeef3fe1aa1dc2
Francisco Barra Sepúlveda <franciscobarra.sep@gmail.com>
2014-11-15 20:48:03 -0300

    j
    j

--- themes/semantic/views/donantes/asigna_alergia.php
+++ themes/semantic/views/donantes/asigna_alergia.php
@@ -1,5 +1,5 @@
-<?php  $this->layout=("//layouts/index");?>
 
+<script src="<?php echo Yii::app()->request->baseUrl; ?>/themes/semantic/packaged/javascript/jquery.js" type="text/javascript"></script> 
 <script src="<?php echo Yii::app()->request->baseUrl; ?>/themes/semantic/packaged/javascript/jquery-ui.js" type="text/javascript"></script> 
 <script src="<?php echo Yii::app()->request->baseUrl; ?>/themes/semantic/packaged/javascript/ui-autocomplete.min.js" type="text/javascript"></script> 
 <link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
