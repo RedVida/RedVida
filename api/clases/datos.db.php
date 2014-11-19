d40c577d8089dac9f047ff60e0a67822889c6295
Francisco Barra Sepúlveda <franciscobarra.sep@gmail.com>
2014-11-19 16:51:07 -0300

    Api
    api

---
+++ api/clases/datos.db.php
@@ -0,0 +1,12 @@
+<?php
+	if(!defined('host')and!defined('user')and!defined('password')and!defined('database')){
+	define('host', '127.0.0.1');
+	define('user', 'root'); //ii_user
+	define('password','');
+	define('database','redvida');		
+	}
+
+	
+	// Se Defininen los las variables host,user,pass én connect()
+
+?>
