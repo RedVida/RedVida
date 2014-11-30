8386b3b048876aa9149267a6b14f13d2a68212d0
hepoblet <hepoblet@alumnos.ubiobio.cl>
2014-11-30 16:19:46 -0300

    ....

--- protected/controllers/DonacionSangreController.php
+++ protected/controllers/DonacionSangreController.php
@@ -159,6 +159,108 @@ class DonacionSangreController extends Controller
 		));
 	}
 
+	public function actionInforme(){
+	// Odernar por: Edad, Tipo de sangre, Centro medico, Fecha de ingreso
+
+		$model = new Donantes;
+		if(isset($_POST['Donantes'])){
+       		$model = new Donantes;
+            $this->layout="//layouts/pdf";
+            $mPDF1 = Yii::app()->ePdf->mpdf();
+			$mPDF1->WriteHTML(CHtml::image(Yii::getPathOfAlias('webroot.css') . '/nn.png' ));
+			$mPDF1->WriteHTML('<br>');
+			$mPDF1->WriteHTML(CHtml::image(Yii::getPathOfAlias('webroot.css') . '/line2.png' ));
+			$mPDF1->WriteHTML('<br> ');
+			$mPDF1->WriteHTML(CHtml::image(Yii::getPathOfAlias('webroot.css') . '/informe_donantes.png' ));
+			$where_array = array();
+			$from_array = array();
+			$OK = true;
+			if($_POST['Donantes']['edad_inicial']!=''){ // edad inicial
+		        if(is_numeric($_POST['Donantes']['edad_inicial'])){
+            		$where_array[]=('d.edad >= '.$_POST['Donantes']['edad_inicial']);
+            	}
+            	else{
+            		$model->addError('nombre','Edad Inicio: No es valido, porfavor ingrese un numero ');
+					$OK = false;	
+            	}
+		    }
+		    if($_POST['Donantes']['edad_final']!=''){ // edad final
+		    	if(is_numeric($_POST['Donantes']['edad_final'])){
+            		$where_array[]=('d.edad <= '.$_POST['Donantes']['edad_final']);
+            	}else{
+            		$model->addError('nombre','Edad Termino: No es valido, porfavor ingrese un numero ');
+					$OK = false;	
+            	}
+		    }
+            if($_POST['Donantes']['id_centro_medico']!=''){ // centro medico
+            	$where_array[]=('d.id_centro_medico = '.$_POST['Donantes']['id_centro_medico']);
+		    }
+		    if($_POST['Donantes']['tipo_sangre']!=''){ // Tipo de sangre
+		    	$length = (string)($_POST['Donantes']['tipo_sangre']);
+            	$where_array[]=('d.tipo_sangre = '."'$length'");
+		    }
+		    if($_POST['Donantes']['desde']!=''){ // Tipo de sangre
+		    	if(strtotime($_POST['Donantes']['desde']) && 1 === preg_match('~[0-9]~', $_POST['Donantes']['desde'])){
+			    	$desde = (string)($_POST['Donantes']['desde']);
+	            	$where_array[]=('d.fecha_ingreso >= '."'$desde'");
+            	}else{
+            		$model->addError('nombre','Fecha de Inicio: La Fecha ingresada no es valida ');
+				   	$OK = false;
+            	}
+		    }
+		     if($_POST['Donantes']['hasta']!=''){ // Fecha
+		     	if(strtotime($_POST['Donantes']['desde']) && 1 === preg_match('~[0-9]~', $_POST['Donantes']['desde'])){
+			    	$hasta = (string)($_POST['Donantes']['hasta']);
+	            	$where_array[]=('d.fecha_ingreso <= '."'$hasta'");
+	            }else{
+            		$model->addError('nombre','Fecha de Termino: La Fecha ingresada no es valida ');
+				   	$OK = false;
+            	}
+		    }
+		    if($_POST['Donantes']['alergia']!=''){ // Alergia
+		    	$length = (string)($_POST['Donantes']['alergia']);
+		    	$modelo = Alergias::model()->findAll(array('select'=>'id,nombre','condition'=>'nombre='."'$length'"));
+		    	if(!$modelo){
+				   $model->addError('nombre','Alergia : El nombre de la Alergia ingresada no existe ');
+				   $OK = false;
+				}else{
+            	$where_array[]=('ta.id_alergia= '.$modelo[0]->id.' AND ta.id_donante = d.id');
+            	$from_array[]=('tiene_alergia ta');
+                }
+		    }
+		    if($_POST['Donantes']['enfermedad']!=''){ // Enfermedades
+		    	$length = (string)($_POST['Donantes']['enfermedad']);
+		    	$modelo = Enfermedades::model()->findAll(array('select'=>'id,nombre','condition'=>'nombre='."'$length'"));
+		    	if(!$modelo){
+				   $model->addError('nombre','Enfermedad : El nombre de la Enfermedad ingresada no existe ');
+				   $OK = false;
+				}else{
+            	$where_array[]=('te.id_enfermedad= '.$modelo[0]->id.' AND te.id_donante = d.id');
+            	$from_array[]=('tiene_enfermedad te');
+                }
+		    }
+		    $form = implode(", ", $from_array);	 
+		    $where = implode(" AND ", $where_array);	
+            $results = Yii::app()->db->createCommand()->
+	            select('*')->
+	            from('donantes d, '.$form)->
+	            where($where)->
+	            queryAll();
+	        if(!$OK)$results =null;
+            if($results){
+				$mPDF1->WriteHTML($this->render('_informe',array('results'=>$results),true));
+				$mPDF1->Output('Informe Dontantes',"I"); // i = visualizar en el navegador
+		    }
+		    else{ $model->addError('nombre','No se han encontrado donantes con esos datos ');}
+        }
+        $this->render('informe',array(
+			'model'=>$model,
+		));
+	}
+
+
+
+
 	/**
 	 * Returns the data model based on the primary key given in the GET variable.
 	 * If the data model is not found, an HTTP exception will be raised.
---
+++ themes/semantic/views/donacionSangre/informe.php
@@ -0,0 +1,166 @@
+<?php  $this->layout="//layouts/index";?>
+<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/themes/semantic/packaged/css/DateTimePicker.css">   
+<link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
+<script src="<?php echo Yii::app()->request->baseUrl; ?>/themes/semantic/packaged/javascript/jquery-ui.js" type="text/javascript"></script> 
+<script src="<?php echo Yii::app()->request->baseUrl; ?>/themes/semantic/packaged/javascript/ui-autocomplete.min.js" type="text/javascript"></script> 
+<script src="<?php echo Yii::app()->request->baseUrl; ?>/themes/semantic/packaged/javascript/DateTimePicker.js" type="text/javascript"></script> 
+
+
+<script type="text/javascript">
+	$(function(){
+        $('#busquedaAlergia').autocomplete({
+       		 source : function( request, response ) {
+       		 $.ajax({
+                    url: '<?php echo $this->createUrl('Alergias/AlergiaList'); ?>',
+                    dataType: "json",
+                    data: { term: request.term },
+                    success: function(data) {
+		                    response($.map(data, function(item) {
+				                    return {
+					                    label: item.nombre,
+					                    }
+					                }))
+                    		}
+        			})
+    		},
+        });
+    });
+    $(function(){
+        $('#busquedaEnfermedad').autocomplete({
+       		 source : function( request, response ) {
+       		 $.ajax({
+                    url: '<?php echo $this->createUrl('Enfermedades/EnfermedadList'); ?>',
+                    dataType: "json",
+                    data: { term: request.term },
+                    success: function(data) {
+		                    response($.map(data, function(item) {
+				                    return {
+					                    label: item.nombre,
+					                    }
+					                }))
+                    		}
+        			})
+    		    },
+    	 });
+    });
+  	$(document).ready(function() {
+			$('.ui.small.modal').modal('attach events','#ModalFunction','show');  //LLamada a Modal UI
+  		});
+
+   
+     $(document).ready(function(){
+              $("#dtBox").DateTimePicker({
+              	dateTimeFormat:"yyyy-MM-dd HH:mm:ss",
+              	shortDayNames: ["Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sab"],
+              	fullDayNames: ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"],
+              	fullMonthNames: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+              	titleContentDateTime: "Seleccionar Hora y Fecha",
+              	setButtonContent: "Agregar",
+              	clearButtonContent: "Borrar",
+              	
+              });
+          });
+      function successModal(){                                                      // Button - Modal Success 
+        $("#informe-form").submit();
+    }
+</script>
+
+<br><br>
+<div class="ui black ribbon label">
+<h1 class="ui huge header add icon"> &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;
+Generar Informe - Donante </h1>
+</div>
+<hr class="style-two ">
+<div class="ui grid">
+	<div class="one wide column"></div>
+		<div class="twelve wide column">
+	 		<div class="form">
+				<?php $form=$this->beginWidget('CActiveForm', array(
+					'id'=>'informe-form',
+					'enableAjaxValidation'=>false,
+				)); ?>
+				<?php Yii::app()->clientScript->registerScript(
+				    'myHideEffect',
+				    '$(".errors").animate({opacity: 1.0}, 5000).fadeOut("slow");',
+				    CClientScript::POS_READY
+				); ?>
+				<?php echo $form->errorSummary($model, NULL, NULL, array("class" => "ui warning message"));?>
+				<div class="ui form">
+					<div class="ui small headerr"><b>* Fecha de ingreso</b></div>
+					<div class="fields">
+					<div class="three wide field">
+						<?php echo $form->labelEx($model,'Desde:'); ?>
+						<?php echo CHtml::textField('Donantes[desde]','',array('data-field'=>'datetime','placeholder'=>'Fecha de inicio')); ?>
+					 </div>
+					 <div class="three wide field">
+						<?php echo $form->labelEx($model,'Hasta:'); ?>
+						<?php echo CHtml::textField('Donantes[hasta]','',array('data-field'=>'datetime','placeholder'=>'Fecha de termino')); ?>
+					 </div>
+ 					<div id="dtBox"></div>
+                    </div>
+                    <div class="ui small headerr"><b>*Rango de edad</b></div>
+					<div class="fields">
+					<div class="two wide field">
+						<?php echo $form->labelEx($model,'Edad Inicio:'); ?>
+						<?php echo CHtml::textField('Donantes[edad_inicial]',''); ?>
+					 </div>
+					 <div class="two wide field">
+						<?php echo $form->labelEx($model,'Edad Termino:'); ?>
+						<?php echo CHtml::textField('Donantes[edad_final]',''); ?>
+					 </div>
+
+                    </div>
+					<div class="four wide field">
+					       <?php echo $form->labelEx($model,'Tipo De Sangre:'); ?>
+					       <?php echo $form->dropDownList($model,'tipo_sangre',CHtml::listData(BancoSangre::model()->findAll(),'tipo', 'tipo'), array('empty' => 'Seleccione Tipo Sangre', 'class'=>'ui selection dropdown')); ?>
+					
+					</div>
+				   	<div class="four wirde field">
+				        <?php echo $form->labelEx($model,'Centro Medico:'); ?>
+						<?php echo $form->dropDownList($model,'id_centro_medico', CHtml::listData(CentroMedico::model()->findAll(),'id', 'nombre'), array('empty' => 'Seleccione Centro Medico', 'class'=>'ui selection dropdown')); ?>
+					</div>
+					<div class="fields">
+						<div class="four wide field">
+								<?php echo $form->labelEx($model,'Alergia:'); ?>
+								<?php echo CHtml::textField('Donantes[alergia]','',array('id'=>'busquedaAlergia','placeholder'=>'Ingrese la Alergia a buscar...')); ?>
+						</div>
+					</div>
+					<div class="fields">
+						 	<div class="four wide field">
+								<?php echo $form->labelEx($model,'Enfermedad:'); ?>
+								<?php echo CHtml::textField('Donantes[enfermedad]','',array('id'=>'busquedaEnfermedad','placeholder'=>'Ingrese la Enfermedad a buscar...')); ?>
+								</div>
+							</div>
+					</div>
+				</div>   
+				   
+					<br>
+						<div class="ui blue submit button" id="ModalFunction"> <!-- Main.PHP -->
+							Filtrar
+				</div>
+			</div>
+		</div>
+<?php $this->endWidget(); ?>
+
+	</div><!-- form -->
+
+
+	 <div class="ui small modal">
+        <i class="close icon"></i>
+          <div class="header">
+            Verificar Operación
+          </div>
+        <div class="content">
+          <i class="large loading icon"></i>
+           Esta seguro que desea registrar estos datos?
+        </div>
+      <div class="actions">
+        <div class="ui negative button" data-value="Cancel" name="Cancel">
+          No
+        </div>
+        <div class="ui positive right labeled icon button"  date-value="Success" onclick="successModal();" name="Success">
+          Si
+          <i class="checkmark icon"></i>
+        </div>
+      </div>
+  </div>
